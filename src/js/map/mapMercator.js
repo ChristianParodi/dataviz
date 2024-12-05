34a5f76b61d4266ccf77d7812d303b7bf1f86006
dariovalentinii <dario.vale01@gmail.com>
2024-12-05 10:59:53 +0100

    fixed zoomed tooltip

--- src/js/map/mapMercator.js
+++ src/js/map/mapMercator.js
@@ -1,6 +1,7 @@
 function mapMercator() {
   const width = 1000;
   const height = 600;
+  zoomed = false;
 
   // Tooltip per mostrare informazioni
   const tooltip = d3.select("body").append("div")
@@ -81,6 +82,26 @@ function mapMercator() {
       .domain(thresholds)
       .range(colors.slice(1));
 
+    function setZoomedTooltip(event, d) {
+      const countryEmissions = emissions.find(r => r.Code == d.id);
+      const emissionsPerCapita = +countryEmissions.CO2;
+      const countryPopulation = +countryEmissions.Population;
+
+      const emissionsType2022 = emissions_type.filter(d => +d.Year === 2022);
+      const fossilEmissions = d3.sum(emissionsType2022.map(d => +d.Fossil)) || 0;
+      const landEmissions = d3.sum(emissionsType2022.map(d => +d.Land)) || 0;
+
+      tooltip.style("opacity", 1)
+        .html(`<strong>${d.properties.name}</strong><br>
+                 Total fossil emissions: ${(fossilEmissions / 1e9).toFixed(3)} Bil t<br />
+                 Total land change emissions: ${(landEmissions / 1e9).toFixed(3)} bil t<br />
+                 CO₂ emissions per capita: ${emissionsPerCapita.toFixed(3).toLocaleString()} t<br />
+                 Population: ${countryPopulation.toLocaleString()} people
+                 `)
+        .style("left", `${event.pageX + 10}px`)
+        .style("top", `${event.pageY + 10}px`);
+    }
+
     // Disegnare la mappa
     g.selectAll("path")
       .data(world.features)
@@ -96,10 +117,15 @@ function mapMercator() {
         const emissions = emissionsByCountry.get(d.id);
 
         // Tooltip
-        tooltip.style("opacity", 1)
-          .html(`<strong>${d.properties.name}</strong><br>${emissions === undefined ? "Data not available" : "Emissions: " + (emissions / 1e9).toFixed(3) + " Bil t"}`)
-          .style("left", `${event.pageX + 10}px`)
-          .style("top", `${event.pageY + 10}px`);
+        if (!zoomed) {
+          tooltip.style("opacity", 1)
+            .html(`<strong>${d.properties.name}</strong><br>${emissions === undefined ? "Data not available" : "Emissions: " + (emissions / 1e9).toFixed(3) + " Bil t"}`)
+            .style("left", `${event.pageX + 10}px`)
+            .style("top", `${event.pageY + 10}px`);
+        }
+        else {
+          setZoomedTooltip(event, d);
+        }
 
         // Highlight the hovered country
         d3.select(this)
@@ -122,13 +148,7 @@ function mapMercator() {
           .attr("stroke-width", 0.5)
           .style("opacity", 1);
       }).on("click", function (event, d) {
-        const countryEmissions = emissions.find(r => r.Code == d.id);
-        const emissionsPerCapita = +countryEmissions.CO2;
-        const countryPopulation = +countryEmissions.Population;
-
-        const emissionsType2022 = emissions_type.filter(d => +d.Year === 2022);
-        const fossilEmissions = d3.sum(emissionsType2022.map(d => +d.Fossil)) || 0;
-        const landEmissions = d3.sum(emissionsType2022.map(d => +d.Land)) || 0;
+        zoomed = true;
 
         // Calculate the bounding box of the clicked country
         const [[x0, y0], [x1, y1]] = path.bounds(d);
@@ -151,59 +171,51 @@ function mapMercator() {
           .duration(750)
           .call(zoom.transform, d3.zoomIdentity.translate(translateX, translateY).scale(scale));
 
-        tooltip.style("opacity", 1)
-          .html(`<strong>${d.properties.name}</strong><br>
-                 Total fossil emissions: ${(fossilEmissions / 1e9).toFixed(3)} Bil t<br />
-                 Total land change emissions: ${(landEmissions / 1e9).toFixed(3)} bil t<br />
-                 CO₂ emissions per capita: ${emissionsPerCapita.toFixed(3).toLocaleString()} t<br />
-                 Population: ${countryPopulation.toLocaleString()} people
-                 `)
-          .style("left", `${event.pageX + 10}px`)
-          .style("top", `${event.pageY + 10}px`);
+        setZoomedTooltip(event, d);
       });
     // Aggiungere effetti di zoom e panoramica
     svg.call(zoom);
-// Create the bins for the legend (based on thresholds and colors)
-const legendBins = thresholds.map((threshold, i) => {
-  return { threshold, color: colors[i], next: thresholds[i + 1] };
-}).slice(0, -1); // Remove the last bin as it doesn't have a corresponding range
+    // Create the bins for the legend (based on thresholds and colors)
+    const legendBins = thresholds.map((threshold, i) => {
+      return { threshold, color: colors[i], next: thresholds[i + 1] };
+    }).slice(0, -1); // Remove the last bin as it doesn't have a corresponding range
 
-// Set dimensions for the legend
-const squareSize = 20; // Size of each square
-const spacing = 20; // Space between squares and text
-const legendPadding = 10; // Padding for the legend container
+    // Set dimensions for the legend
+    const squareSize = 20; // Size of each square
+    const spacing = 20; // Space between squares and text
+    const legendPadding = 10; // Padding for the legend container
 
-// Define a scale for the widths of the legend squares based on the range
-const maxRange = d3.max(legendBins, d => d.next - d.threshold); // Maximum range
-const squareWidthScale = d3.scaleLinear()
-  .domain([0, maxRange]) // Input domain is the range of emission values
-  .range([20, 120]); // Output range for square widths (min to max length)
+    // Define a scale for the widths of the legend squares based on the range
+    const maxRange = d3.max(legendBins, d => d.next - d.threshold); // Maximum range
+    const squareWidthScale = d3.scaleLinear()
+      .domain([0, maxRange]) // Input domain is the range of emission values
+      .range([20, 120]); // Output range for square widths (min to max length)
 
-  let cumulativeX = legendPadding; // Inizializza la posizione iniziale
+    let cumulativeX = legendPadding; // Inizializza la posizione iniziale
 
-  legendSvg.selectAll("rect")
-    .data(legendBins)
-    .join("rect")
-    .attr("x", (d, i) => {
-      const currentX = cumulativeX; // Memorizza la posizione corrente
-      cumulativeX += squareWidthScale(d.next - d.threshold) + spacing; // Aggiorna la posizione cumulativa
-      return currentX; // Ritorna la posizione corrente per l'elemento
-    })
-    .attr("y", 10) // Posizione verticale costante
-    .attr("width", d => squareWidthScale(d.next - d.threshold)) // Larghezza proporzionale
-    .attr("height", squareSize) // Altezza costante
-    .style("fill", d => d.color); // Colore basato sulla scala
+    legendSvg.selectAll("rect")
+      .data(legendBins)
+      .join("rect")
+      .attr("x", (d, i) => {
+        const currentX = cumulativeX; // Memorizza la posizione corrente
+        cumulativeX += squareWidthScale(d.next - d.threshold) + spacing; // Aggiorna la posizione cumulativa
+        return currentX; // Ritorna la posizione corrente per l'elemento
+      })
+      .attr("y", 10) // Posizione verticale costante
+      .attr("width", d => squareWidthScale(d.next - d.threshold)) // Larghezza proporzionale
+      .attr("height", squareSize) // Altezza costante
+      .style("fill", d => d.color); // Colore basato sulla scala
 
-// Add labels beside each square to show the range
-legendSvg.selectAll("text")
-  .data(legendBins)
-  .join("text")
-  .attr("x", (d, i) => legendPadding + i * (squareSize + spacing) ) // Position text to the right of the square
-  .attr("y", d => 50  ) // Align text with the middle of each square
-  .attr("dy", "0.35em") // Center the text vertically
-  .text(d => `${(d.threshold / 1e9).toFixed(2)} - ${(d.next / 1e9).toFixed(2)} bil t`) // Format the range
-  .style("font-size", "10px")
-  .style("fill", "#333");
+    // Add labels beside each square to show the range
+    legendSvg.selectAll("text")
+      .data(legendBins)
+      .join("text")
+      .attr("x", (d, i) => legendPadding + i * (squareSize + spacing)) // Position text to the right of the square
+      .attr("y", d => 50) // Align text with the middle of each square
+      .attr("dy", "0.35em") // Center the text vertically
+      .text(d => `${(d.threshold / 1e9).toFixed(2)} - ${(d.next / 1e9).toFixed(2)} bil t`) // Format the range
+      .style("font-size", "10px")
+      .style("fill", "#333");
 
 
 
