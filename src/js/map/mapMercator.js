fb4387195a7b35cc6ced62806654bfbf110745e1
Christian Parodi <75328764+ChristianParodi@users.noreply.github.com>
2024-12-06 14:08:46 +0100

    migliorata legenda

--- src/js/map/mapMercator.js
+++ src/js/map/mapMercator.js
@@ -65,7 +65,7 @@ function mapMercator() {
   const g = svg.append("g");
 
   // Creare una seconda SVG per la legenda
-  const legendWidth = 1000;
+  const legendWidth = window.innerWidth;
   const legendHeight = 100;
 
   const legendSvg = d3.select("#legend-container-mercator")
@@ -82,12 +82,8 @@ function mapMercator() {
   ]).then(([world, emissions, emissions_type]) => {
     const emissionsByCountry = new Map(emissions.map(d => [d.Code, +d.Total]));
     const emissionsPerCapitaByCountry = new Map(emissions.map(d => [d.Code, +d.CO2]));
-    const emissionDomain = [
-      d3.min(emissions, d => +d.Total),
-      d3.max(emissions, d => +d.Total),
-    ];
     const emissionsValues = emissions.map(d => +d.Total).sort((a, b) => a - b);
-
+    const perCapitaEmissionsValues = emissions.map(d => +d.CO2).sort((a, b) => a - b);
     // Colori per la scala
     const thresholds = [
       0,
@@ -100,7 +96,6 @@ function mapMercator() {
       5e9,
       6e9,
       8e9,
-      10e9,
       emissionsValues[emissionsValues.length - 1]
     ];
 
@@ -110,7 +105,7 @@ function mapMercator() {
 
     const colorScaleTotal = d3.scaleThreshold()
       .domain(thresholds)
-      .range(colors.slice(1));
+      .range(colors.slice(0));
 
     function setZoomedTooltip(event, d) {
       const countryEmissions = emissions.find(r => r.Code == d.id);
@@ -140,7 +135,7 @@ function mapMercator() {
       }
     }
 
-    const perCapitaThresholds = [0, 1, 2, 5, 10, 20, 30, 50];
+    const perCapitaThresholds = [0, 1, 2, 5, 10, 20, 30, 40];
     const colorScalePerCapita = d3.scaleThreshold()
       .domain(perCapitaThresholds)
       .range(d3.schemeOranges[9]);
@@ -241,111 +236,134 @@ function mapMercator() {
       svg.call(zoom);
     }
 
-    // Initialize the map with total emissions
+    function updateLegend() {
+      const isPerCapita = document.getElementById("toggle-mercator").checked;
+      const scale = isPerCapita ? colorScalePerCapita : colorScaleTotal;
+
+      const selectedThreshold = isPerCapita ? perCapitaThresholds : thresholds;
+      // Create the bins for the legend (based on thresholds and colors)
+      let legendBins = selectedThreshold.map((threshold, i) => {
+        return { threshold, color: scale(threshold), next: selectedThreshold[i + 1] };
+      }).slice(0, -1); // Remove the last bin as it doesn't have a corresponding range
+
+      legendBins.unshift({ threshold: undefined, color: "#ccc", next: undefined });
+
+      // Set dimensions for the legend
+      const squareSize = 20; // Size of each square
+      const spacing = 15; // Space between squares and text
+      const legendPadding = 30; // Padding for the legend container
+
+      // Define a scale for the widths of the legend squares based on the range
+      const squareWidthScale = d3.scaleLinear()
+        .domain([0, selectedThreshold[selectedThreshold.length - 1]]) // Input domain is the range of emission values
+        .range([0, legendWidth - 2 * legendPadding - 2 * squareSize - 2 * spacing]); // Output range for square widths (min to max length)
+      console.log(selectedThreshold[selectedThreshold.length - 1])
+      let cumulativeX = legendPadding; // Inizializza la posizione iniziale
+
+      legendSvg.selectAll("rect")
+        .data(legendBins)
+        .join("rect")
+        .attr("x", (d, i) => {
+          const currentX = cumulativeX; // Memorizza la posizione corrente
+          if (i > 1) { // after the gray and the 0.00 - 0.01, do it normally
+            cumulativeX += squareWidthScale(d.next - d.threshold); // Aggiorna la posizione cumulativa
+          }
+          else
+            cumulativeX += squareSize + spacing
+          return currentX; // Ritorna la posizione corrente per l'elemento
+        })
+        .attr("y", 10) // Posizione verticale costante
+        .attr("width", (d, i) => {
+          return i > 1 ? squareWidthScale(d.next - d.threshold) : squareSize;
+        }) // Larghezza proporzionale
+        .attr("height", squareSize) // Altezza costante
+        .attr("style", "outline: 0.05px solid orange;")
+        .style("fill", d => d.color) // Colore basato sulla scala
+        .on("mousemove", function (event, d) {
+          // Tooltip
+          tooltip.style("opacity", 1)
+            .html(`</strong>${"Emissions: " + (d.threshold / 1e9).toFixed(2)} - ${(d.next / 1e9).toFixed(2) + " Bil t"}`)
+            .style("left", `${event.pageX + 10}px`)
+            .style("top", `${event.pageY + 10}px`);
+        })
+        .on("mouseout", function () {
+          tooltip.style("opacity", 0);
+
+        });
+
+      legendSvg.selectAll("text").remove();
+
+      cumulativeX = legendPadding; // Reinizializza per i testi
+      legendSvg.selectAll(".bar-text")
+        .data(legendBins.slice(0)) // I numeri per le barrette
+        .join("text")
+        .attr("x", (d, i) => {
+          let currentX = cumulativeX;
+          if (i > 1) { // after the gray and the 0.00 - 0.01, do it normally
+            if (i === 2)
+              currentX -= squareSize * 0.5;
+            cumulativeX = currentX + squareWidthScale(d.next - d.threshold); // Aggiorna la posizione cumulativa
+            return currentX; // Ritorna la posizione corrente per l'elemento
+          }
+          else {
+            if (i === 0)
+              currentX += squareSize * 0.5; // Memorizza la posizione corrente
+            cumulativeX = currentX + squareSize + spacing
+            return currentX; // Ritorna la posizione corrente per l'elemento
+          }
+        })
+        .attr("y", 45) // Posizione sopra i quadratini
+        .text((d, i) => {
+          if (i === 0) // gray
+            return "Nd";
+
+          if (i === 1 && isPerCapita) // 0 - 0.01
+            return "<0.01";
+
+          return (isPerCapita ? legendBins[i].threshold : legendBins[i].threshold / 1e9).toFixed(2)
+        }) // Mostra il valore di tonnellate
+        .style("font-size", "10px")
+        .style("fill", "#333")
+        .style("text-anchor", "middle");
+
+
+      legendSvg.append("text")
+        .attr("x", cumulativeX) // Posizione finale dopo l'ultimo quadratino
+        .attr("y", 45)
+        .text((legendBins[legendBins.length - 1].next / 1e9).toFixed(2)) // Ultimo valore (d.next)
+        .style("font-size", "10px")
+        .style("fill", "#333")
+        .style("text-anchor", "middle");
+
+      // Primo tick (iniziale)
+      legendSvg.append("rect")
+        .attr("x", legendPadding + 2 * spacing + 2 * squareSize - 1) // Posizione iniziale del primo tick
+        .attr("y", 10)
+        .attr("width", 2)
+        .attr("height", 25)
+        .attr("fill", "orange")
+        .attr("stroke-width", "2px");
+
+      // Ultimo tick (finale)
+      legendSvg.append("rect")
+        .attr("x", cumulativeX - 1) // Posizione finale dopo l'ultimo rettangolo
+        .attr("y", 10)
+        .attr("width", 2)
+        .attr("height", 25)
+        .attr("fill", "orange")
+        .attr("stroke-width", "2px");
+
+      // Append the axis to the legend
+      legendSvg.append("g")
+        .attr("transform", `translate(60, 0)`) // Position the axis beside the legend rectangles
+        .call(g => g.select(".domain").remove()); // Remove the line of the axis
+    }
+
     updateMap();
+    updateLegend();
 
-    // Add event listener for the toggle switch
     document.getElementById("toggle-mercator").addEventListener("change", updateMap);
-
-    // Create the bins for the legend (based on thresholds and colors)
-    const legendBins = thresholds.map((threshold, i) => {
-      return { threshold, color: colors[i], next: thresholds[i + 1] };
-    }).slice(0, -1); // Remove the last bin as it doesn't have a corresponding range
-
-    // Set dimensions for the legend
-const squareSize = 20; // Size of each square
-const spacing = 0; // Space between squares and text
-const legendPadding = 10; // Padding for the legend container
-
-// Define a scale for the widths of the legend squares based on the range
-const maxRange = d3.max(legendBins, d => d.next - d.threshold); // Maximum range
-const squareWidthScale = d3.scaleLinear()
-  .domain([0, maxRange]) // Input domain is the range of emission values
-  .range([20, 120]); // Output range for square widths (min to max length)
-
-  let cumulativeX = legendPadding+95; // Inizializza la posizione iniziale
-
-  legendSvg.selectAll("rect")
-    .data(legendBins)
-    .join("rect")
-    .attr("x", (d, i) => {
-      const currentX = cumulativeX; // Memorizza la posizione corrente
-      cumulativeX += squareWidthScale(d.next - d.threshold) + spacing; // Aggiorna la posizione cumulativa
-      return currentX; // Ritorna la posizione corrente per l'elemento
-    })
-    .attr("y", 10) // Posizione verticale costante
-    .attr("width", d => squareWidthScale(d.next - d.threshold)) // Larghezza proporzionale
-    .attr("height", squareSize) // Altezza costante
-    .attr("style", "outline: 0.05px solid orange;") 
-    .style("fill", d => d.color) // Colore basato sulla scala
-    .on("mousemove", function (event, d) {
-      const emissions = emissionsByCountry.get(d.id);
-
-      // Tooltip
-      tooltip.style("opacity", 1)
-        .html(`</strong>${ "Emissions: " + (d.threshold / 1e9).toFixed(2)} - ${(d.next / 1e9).toFixed(2) + " Bil t"}`)
-        .style("left", `${event.pageX + 10}px`)
-        .style("top", `${event.pageY + 10}px`);
-      })
-      .on("mouseout", function () {
-        tooltip.style("opacity", 0);
-
-    });
-
-  let cumulativeZ = legendPadding+95; // Reinizializza per i testi
-legendSvg.selectAll(".bar-text")
-  .data(legendBins.slice(0)) // I numeri per le barrette
-  .join("text")
-  .attr("x", (d, i) => {
-    const currentX = cumulativeZ; // Memorizza la posizione corrente
-    cumulativeZ += squareWidthScale(d.next - d.threshold); // Aggiorna la posizione cumulativa
-    return currentX; // Ritorna la posizione corrente per l'elemento
-  })
-  .attr("y", 45) // Posizione sopra i quadratini
-  .text((d, i) => i === 0 ? "0" :(legendBins[i].threshold / 1e9).toFixed(2)) // Mostra il valore di tonnellate
-  .style("font-size", "10px")
-  .style("fill", "#333")
-  .style("text-anchor", "middle");
-
-  legendSvg.append("text")
-  .attr("x", cumulativeZ - spacing) // Posizione finale dopo l'ultimo quadratino
-  .attr("y", 45)
-  .text((legendBins[legendBins.length - 1].next / 1e9).toFixed(2)) // Ultimo valore (d.next)
-  .style("font-size", "10px")
-  .style("fill", "#333")
-  .style("text-anchor", "middle");
-
-  // Primo tick (iniziale)
-legendSvg.append("rect")
-.attr("x", legendPadding + 94) // Posizione iniziale del primo tick
-.attr("y", 10)
-.attr("width", 2)
-.attr("height", 25)
-.attr("fill", "orange")
-.attr("stroke-width", "2px");
-
-// Ultimo tick (finale)
-legendSvg.append("rect")
-.attr("x", cumulativeX-1) // Posizione finale dopo l'ultimo rettangolo
-.attr("y", 10)
-.attr("width", 2)
-.attr("height", 25)
-.attr("fill", "orange")
-.attr("stroke-width", "2px");
-
-    // Scale for the legend axis (match the vertical position of rectangles)
-    const legendScale = d3.scaleLinear()
-      .domain([0, thresholds.length - 1]) // Map indices to the range of bins
-      .range([(height - legendHeight) / 2 + legendHeight, (height - legendHeight) / 2]); // Fit within the legend height
-
-
-    // Append the axis to the legend
-    legendSvg.append("g")
-      .attr("transform", `translate(60, 0)`) // Position the axis beside the legend rectangles
-      .call(legendAxis)
-      .call(g => g.select(".domain").remove()); // Remove the line of the axis
-
-
+    document.getElementById("toggle-mercator").addEventListener("change", updateLegend);
   });
 }
 
